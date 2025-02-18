1f126748d80549d63b1327f8c2d2cc8cfa3fc9e8
Serhii Tyshchenko <hexzon@gmail.com>
2025-02-18 16:56:18 +0100

    all packages added

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Flex, Text, Button } from "@radix-ui/themes";
+
 
 function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <Flex direction="column" gap="2">
+        <Text>Hello from Radix Themes :)</Text>
+        <Button>Let's go</Button>
+      </Flex>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import "@radix-ui/themes/styles.css";
+import {
+  Theme,
+  // ThemePanel 
+} from "@radix-ui/themes";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Theme grayColor="sand" panelBackground="solid" radius="none" scaling="95%" appearance='dark'>
+      <App />
+      {/* <ThemePanel /> */}
+    </Theme>
   </StrictMode>,
 )
