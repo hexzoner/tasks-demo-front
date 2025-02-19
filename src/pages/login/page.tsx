5023b6a602cf82dd6dd142a9d842ea06540af1d5
Serhii Tyshchenko <hexzon@gmail.com>
2025-02-19 17:26:52 +0100

    login form

--- src/pages/login/page.tsx
+++ src/pages/login/page.tsx
@@ -1,19 +1,30 @@
 import { FC } from 'react';
-import { Flex, TextField, Section, Button } from "@radix-ui/themes"
+import { useForm } from "react-hook-form";
+import { Flex, Section, Button } from "@radix-ui/themes"
+import { Form } from "radix-ui";
 import { sLoginSection, sLoginPage, sButtonSubmit } from "../../styles/styles"
 import LoadingSpinner from '../../components/LoadingSpinner';
 import { loginMutation } from '../../api/auth';
 
-const loginData = {
-    email: "aaa",
-    password: "1234"
-}
+// const loginData = {
+//     email: "aaa",
+//     password: "1234"
+// }
+
+const textError = "text-red-500 text-xs text-left mt-0 ml-2 absolute"
 
 export const Page: FC = () => {
     const login = loginMutation()
 
-    function handleLogin() {
-        login.mutate(loginData)
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<{ email: string; password: string }>();
+
+    function handleLogin(data: { email: string; password: string }) {
+        console.log(data)
+        login.mutate(data)
     }
 
     if (login.isPending) return <LoadingSpinner />
@@ -21,15 +32,52 @@ export const Page: FC = () => {
     return <div className='min-h-screen ' style={sLoginPage}>
         <Section style={sLoginSection} className='w-full'>
             <p style={{ textAlign: "center", fontSize: "1.5rem", padding: "1rem" }}>Login</p>
-            <Flex direction="column" gap="3" maxWidth="250px" style={{ margin: "auto" }}>
-                <TextField.Root color="gray" variant="soft" placeholder="Email Address" />
-                <TextField.Root color="gray" variant="soft" placeholder="Password" />
-                <Button onClick={handleLogin} color="green" variant="soft" style={sButtonSubmit}>Submit</Button>
-                <div className='text-center'>
-                    {login.isError ? (<div className='text-red-400 font-thin text-sm'>{login.error.response.data.errors[0].name} - {login.error.response.data.errors[0].message}</div>) : null}
-                    {login.isSuccess ? (<div className='text-green-400 font-thin text-sm'>Login successful</div>) : null}
-                </div>
-            </Flex>
+            <Form.Root onSubmit={handleSubmit(handleLogin)} className="FormRoot">
+                <Flex direction="column" gap="5" maxWidth="250px" style={{ margin: "auto" }}>
+
+                    <Form.Field className="FormField" name="email">
+                        <div style={{ display: "flex", alignItems: "baseline", justifyContent: "space-between", }}>
+                            <Form.Label className="FormLabel">Email</Form.Label>
+                        </div>
+                        <Form.Control asChild>
+                            <input
+                                {...register("email", {
+                                    required: "Email is required",
+                                    pattern: {
+                                        value: /^\S+@\S+$/i,
+                                        message: "Invalid email address",
+                                    },
+                                })}
+                                className="Input bg-[#282f3d] px-2 py-1 text-sm font-light w-full my-1" type="email" required placeholder='Enter your email...' />
+                        </Form.Control>
+                        {errors.email && <p className={textError}>{errors.email.message?.toString()}</p>}
+                    </Form.Field>
+
+                    <Form.Field className="FormField" name="password">
+                        <div style={{ display: "flex", alignItems: "baseline", justifyContent: "space-between", }}>
+                            <Form.Label className="FormLabel">Password</Form.Label>
+                        </div>
+                        <Form.Control asChild>
+                            <input
+                                {...register("password", {
+                                    required: "Password is required",
+                                    minLength: {
+                                        value: 4,
+                                        message: "Password must be at least 4 characters",
+                                    },
+                                })}
+                                className="Input bg-[#282f3d] px-2 py-1 text-sm font-light w-full my-1" type="password" required placeholder='Enter your password...' />
+                        </Form.Control>
+                        {errors.password && <p className={textError}>{errors.password.message?.toString()}</p>}
+                    </Form.Field>
+
+                    <Button type='submit' color="green" variant="soft" style={sButtonSubmit}>Submit</Button>
+                    <div className='text-center'>
+                        {login.isError ? (<div className='text-red-400 font-thin text-sm'>Error: {login.error.response.data.errors[0].message}</div>) : null}
+                        {login.isSuccess ? (<div className='text-green-400 font-thin text-sm'>Login successful</div>) : null}
+                    </div>
+                </Flex>
+            </Form.Root>
         </Section>
     </div >
 };
